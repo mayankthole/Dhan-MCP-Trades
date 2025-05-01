844e2d061e8f724d91b398ae7ba6a087f10a23d0
mayank thole <119438990+mayankthole@users.noreply.github.com>
2025-05-01 14:02:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 # Add your DhanHQ credentials here
 
 # Your DhanHQ client ID
-DHAN_CLIENT_ID = "1106534888"
+DHAN_CLIENT_ID = " "
 
 # Your DhanHQ access token
-DHAN_ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzQ3NjQ2MDMzLCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwNjUzNDg4OCJ9.TrT8U_uS3TEqqF23VGBgc1_SHk9f3S0e6yp_tbRdZ97A_93bZuYNcUul9JvGxme4_8bd3rvgyUnuzdNa9Y8QYA"
+DHAN_ACCESS_TOKEN = " "
 
 # API Base URL
 DHAN_API_BASE_URL = "https://api.dhan.co/v2"
